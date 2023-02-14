9da22c9ecc38fe00b59f35a1f699fbf3fd33c4c5
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-02-14 18:27:32 +0530

    Create (27-01-2023) 472. Concatenated Words.py

---
+++ January 2023/(27-01-2023) 472. Concatenated Words.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        s = set()
+        concatenateWords = []
+        for word in words:
+            s.add(word)
+        for word in words:
+            if self.checkConcatenate(word, s) == True:
+                concatenateWords.append(word)
+        return concatenateWords
+    
+    def checkConcatenate(self, word: str, s: set) -> bool:
+        for i in range(1, len(word)):
+            prefixWord = word[:i]
+            suffixWord = word[i:]
+            if prefixWord in s and (suffixWord in s or self.checkConcatenate(suffixWord, s)):
+                return True
+        return False
