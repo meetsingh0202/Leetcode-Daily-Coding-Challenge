5ad4166e637f477e20d9ec73b121073987462b3f
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-05-16 19:16:19 +0530

    Time: 253 ms (26.21%), Space: 21.8 MB (31.91%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,86 +1,79 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        em_to_name = {}
-        em_graph = defaultdict(set)
         
-        for acc in accounts:
-            name = acc[0]
-            for email in acc[1:]:
-                em_graph[acc[1]].add(email)
-                em_graph[email].add(acc[1])
-                em_to_name[email] = name
+        def union(x, y):
+            # print('-----------')
+            # print(p)
+            # print(x, y)
+            
+            pX, pY = find(x), find(y)
+            
+            if pY in total:
+                p[pX] = pY
+                p[x] = pY
+                total[pY].add(x)
+            else:
+                p[pY] = p[pX]
+                p[y] = pX
+                total[pX].add(y)
+            
+        def find(x):
+            if x not in p:
+                p[x] = x
+                return p[x]
+                
+            if x != p[x]:
+                p[x] = find(p[x])
+            return p[x]
+        
+        res = []
+        p = dict()
+        total = defaultdict(set)
+        
+        adjacencyList = defaultdict(list)
+        ownerName = dict()
+        
+        for i in accounts:
+            name = i[0]
+            if len(i) > 2:
+                currEmail = i[1]
+                ownerName[currEmail] = name
+                for index in range(2, len(i)):
+                    nextEmail = i[index]
+                    if nextEmail == currEmail:
+                        continue
+                    ownerName[nextEmail] = name
+                    if find(currEmail) != find(nextEmail):
+                        if nextEmail in total:
+                            union(nextEmail, currEmail)
+                        else:
+                            union(currEmail, nextEmail)
+            else:
+                ownerName[i[1]] = name
+                union(i[1], i[1])
+                
+        count = set()
+        res = []
+        HashMap = defaultdict(set)
+        
+        for key, val in p.items():
+            HashMap[find(val)].add(key)
+        
+        for key, val in HashMap.items():
+            name = ownerName[key]
+            val.add(key)
+            tempList = list(val)
+            res.append([name] + sorted(tempList))
+        return res
     
-        seen = set()
-        ans = []
-        for email in em_graph:
-            if email not in seen:
-                seen.add(email)
-                q = [email]
-                component = []
-                while q:
-                    edge = q.pop(0)
-                    component.append(edge)
-                    for nei in em_graph[edge]:
-                        if nei not in seen:
-                            seen.add(nei)
-                            q.append(nei)
-                ans.append([em_to_name[email]] + sorted(component))
-        return ans
-#         def union(x, y):
-#             print('-----------')
-#             print(p)
-#             print(x, y)
-            
-#             if p[y] != y:
-#                 p[find(x)] = p[find(y)]
-#                 total[find(y)].add(x)
-#             else:
-#                 p[find(y)] = p[find(x)]
-#                 total[find(x)].add(y)
-            
-#         def find(x):
-#             if x not in p:
-#                 p[x] = x
-#                 return p[x]
-                
-#             if x != p[x]:
-#                 p[x] = find(p[x])
-#             return p[x]
-        
-#         res = []
-#         p = dict()
-#         total = defaultdict(set)
-        
-#         adjacencyList = defaultdict(list)
-#         ownerName = dict()
-        
-#         for i in accounts:
-#             name = i[0]
-#             if len(i) > 2:
-#                 currEmail = i[1]
-#                 ownerName[currEmail] = name
-#                 for index in range(2, len(i)):
-#                     nextEmail = i[index]
-#                     ownerName[nextEmail] = name
-#                     if find(currEmail) != find(nextEmail):
-#                         union(currEmail, nextEmail)
-#             else:
-#                 ownerName[i[1]] = name
-#                 union(i[1], i[1])
-                
-#         count = set()
-#         res = []
-        
-#         for key, val in total.items():
-#             temp = []
-#             name = ownerName[key]
-#             if len(val) > 0:
-#                 val.add(key)
-#                 temp.append(name)
-#                 temp.extend(sorted(list(val)))
-#             else:
-#                 temp.append(name)
-#                 temp.append(key)
-#             res.append(temp)
-#         return res
+    """
+    [["Hanzo","Hanzo2@m.co","Hanzo3@m.co"],["Hanzo","Hanzo4@m.co","Hanzo5@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co"],["Hanzo","Hanzo3@m.co","Hanzo4@m.co"],["Hanzo","Hanzo7@m.co","Hanzo8@m.co"],["Hanzo","Hanzo1@m.co","Hanzo2@m.co"],["Hanzo","Hanzo6@m.co","Hanzo7@m.co"],["Hanzo","Hanzo5@m.co","Hanzo6@m.co"]]
+    [["David","David0@m.co","David5@m.co","David0@m.co"],["Lily","Lily4@m.co","Lily2@m.co","Lily0@m.co"],["Fern","Fern5@m.co","Fern2@m.co","Fern6@m.co"],["Gabe","Gabe0@m.co","Gabe6@m.co","Gabe8@m.co"],["Alex","Alex7@m.co","Alex5@m.co","Alex7@m.co"],["Lily","Lily4@m.co","Lily6@m.co","Lily7@m.co"],["Alex","Alex0@m.co","Alex4@m.co","Alex5@m.co"],["John","John4@m.co","John2@m.co","John0@m.co"]]
+    [["David","David0@m.co","David1@m.co"],["David","David3@m.co","David4@m.co"],["David","David4@m.co","David5@m.co"],["David","David2@m.co","David3@m.co"],["David","David1@m.co","David2@m.co"]]
+    [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+[["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+[["Alex","Alex5@m.co","Alex4@m.co","Alex0@m.co"],["Ethan","Ethan3@m.co","Ethan3@m.co","Ethan0@m.co"],["Kevin","Kevin4@m.co","Kevin2@m.co","Kevin2@m.co"],["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe2@m.co"],["Gabe","Gabe3@m.co","Gabe4@m.co","Gabe2@m.co"]]
+    
+    """
+    
     
