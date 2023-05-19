4a5e2eb3bc1c851f33e0a851445737710e6355a1
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-05-19 15:36:01 +0530

    Time: 85 ms (28.90%), Space: 22.7 MB (25.17%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -1,33 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class BSTIterator:
-    def printInorder(self, root):
-        if root:
-            self.printInorder(root.left)
-            self.arr.append(root.val),
-            self.printInorder(root.right)
-            
     def __init__(self, root):
-        self.arr = [0]
-        self.root = root
-        self.printInorder(self.root)
-        self.index = 0
+        self.stack = list()
+        self.pushAll(root)
         
-    def next(self):
-        self.index += 1
-        currVal = self.arr[self.index]
-        return currVal
-    
     def hasNext(self):
-        if self.index >= len(self.arr) - 1:
-            return False
-        return True
+        return self.stack
 
-# Your BSTIterator object will be instantiated and called as such:
-# obj = BSTIterator(root)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
+    def next(self):
+        tmpNode = self.stack.pop()
+        self.pushAll(tmpNode.right)
+        return tmpNode.val
+        
+    def pushAll(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
