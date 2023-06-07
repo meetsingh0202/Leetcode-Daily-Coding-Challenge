d4a802704f89dc83661ae4d73414b97cabd16e31
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-06-07 18:20:19 +0530

    Time: 61 ms (85.98%), Space: 16.9 MB (57.75%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -1,22 +1,22 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        
+        @cache
         def traverse(s):
-            if sum(s) == 0:
+            if s == '0' * len(s):
                 return 0
 
-            if (tuple(s) in memo):
-                return memo[tuple(s)]
+            if (s in memo):
+                return memo[s]
             
             count = 0
         
             for index in range(len(s)):
-                if s[index]:
-                    s[index] -= 1
+                if s[index] != '0':
+                    s = s[:index] + str(int(s[index]) - 1) + s[index + 1: ]
                     count += (1 + traverse(s))
-                    s[index] += 1
+                    s = s[:index] + str(int(s[index]) + 1) + s[index + 1: ]
                     
-            memo[tuple(s)] = count
+            memo[s] = count
             return count
         
         n = len(tiles)
@@ -26,5 +26,9 @@ class Solution:
             HashMap[i] = 1 + HashMap.get(i, 0)
             
         memo = dict()
+        currS = ""
         
-        return traverse(list(HashMap.values()))
+        for i in HashMap.values():
+            currS += str(i)
+            
+        return traverse(currS)
