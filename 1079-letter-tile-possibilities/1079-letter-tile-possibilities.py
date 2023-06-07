0ddb23abbb788868d72a05c4574d0c65d1a800ee
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-06-07 18:16:11 +0530

    Time: 46 ms (95.76%), Space: 16.6 MB (58.12%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -1,21 +1,23 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         
-        
         def traverse(s):
-            nonlocal count
-            
             if sum(s) == 0:
-                count += 1
-                return
+                return 0
+
+            if (tuple(s) in memo):
+                return memo[tuple(s)]
             
-            count += 1
+            count = 0
         
             for index in range(len(s)):
                 if s[index]:
                     s[index] -= 1
-                    traverse(s)
+                    count += (1 + traverse(s))
                     s[index] += 1
+                    
+            memo[tuple(s)] = count
+            return count
         
         n = len(tiles)
         HashMap = dict()
@@ -23,7 +25,6 @@ class Solution:
         for i in tiles:
             HashMap[i] = 1 + HashMap.get(i, 0)
             
-        count = 0
-        traverse(list(HashMap.values()))
+        memo = dict()
         
-        return count - 1
+        return traverse(list(HashMap.values()))
