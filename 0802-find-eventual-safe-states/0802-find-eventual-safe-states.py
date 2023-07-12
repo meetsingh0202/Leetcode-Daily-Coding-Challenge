52e5017d6474a9d0900c53a799a919abc32b2bf5
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-07-12 09:46:44 +0530

    Time: 686 ms (48.57%), Space: 24.3 MB (35.19%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,40 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        
+        def traverse(currNode, lastNode):
+            count[currNode] = 0
+            visited.add(currNode)
+            
+            for i in graph[currNode]:
+                if i not in visited:
+                    if traverse(i, currNode):
+                        pass
+                    else:
+                        count[currNode] += 1
+                        break
+                else:
+                    if isSafe[i] == True:
+                        continue
+                    count[currNode] += 1
+                        
+            flag = True if count[currNode] == 0 else False
+            isSafe[currNode] = flag
+            
+            # print(currNode, isSafe[currNode], count[currNode])
+            
+            return isSafe[currNode]
+        
+        visited = set()
+        isSafe = [False] * len(graph)
+        count = [0] * len(graph)
+        res = []
+        
+        for i in range(len(graph)):
+            if i not in visited:
+                traverse(i, -1)
+        
+        for i in range(len(graph)):
+            if isSafe[i]:
+                res.append(i)
+        
+        return res
