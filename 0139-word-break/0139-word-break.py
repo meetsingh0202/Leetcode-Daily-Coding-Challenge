b55e189d6be1c4ee834ae4152adfa779113edcab
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-08-04 09:10:44 +0530

    Time: 52 ms (64.41%), Space: 16.4 MB (83.50%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        visited = set()
+        queue = deque([0])
+        while queue:
+            start = queue.popleft()
+            if start not in visited:
+                for end in range(start + 1, len(s)+1):
+                    if s[start:end] in wordDict:
+                        queue.append(end)
+                        if end == len(s):
+                            return True
+                visited.add(start)
+        return False
