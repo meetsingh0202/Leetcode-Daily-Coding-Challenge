ab1185c10214f968aa226e85a94ed5468d04c9db
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-08-06 22:09:48 +0530

    Time: 44 ms (96.24%), Space: 17.5 MB (32.33%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        @cache
+        def fn(i, x): 
+            if i == goal: 
+                return x == n 
+            ans = 0 
+            if x < n:
+                ans += (n-x) * fn(i+1, x+1)
+            if k < x:
+                ans += (x-k) * fn(i+1, x)
+            return ans % 1_000_000_007
+        
+        return fn(0, 0)
