194eaaab051622d9361e3baf223fc387b3f4fc94
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-10-17 09:51:35 +0530

    Time: 267 ms (69.46%), Space: 18.3 MB (88.32%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,6 +1,30 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         
+        s = [0]
+        visited = defaultdict(bool)
+        
+        for i in range(n):
+            if leftChild[i] == s[0] or rightChild[i] == s[0]:
+                s[0] = i
+                       
+        while s:
+            node = s.pop(0)
+                
+            if visited[node]: 
+                return False
+            
+            n -= 1
+            visited[node] = True
+            
+            if leftChild[node] != -1:
+                s.append(leftChild[node])
+            if rightChild[node] != -1:
+                s.append(rightChild[node])
+        
+        return n == 0
+        
+    def validateBinaryTreeNodes1(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         def findRoot():
             HashSet = set()
             
