49f91b74e219bf237daa6ba8fb4106b68f84e8ca
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-10-18 06:57:45 +0530

    Time: 1166 ms (64.11%), Space: 24.1 MB (79.18%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        
+        HashMap = dict()
+        count = 0
+        MOD = 1000000007
+        deliciousness.sort()
+        
+        for i in deliciousness:
+            currNum = i
+            
+            for k in range(22):
+                temp = 1 << k
+                
+                y = temp - currNum
+                
+                if y > currNum:
+                    break
+                
+                count += HashMap.get(y, 0)
+                count = count % MOD
+                
+            HashMap[i] = 1 + HashMap.get(i, 0)
+            
+        return count % MOD
