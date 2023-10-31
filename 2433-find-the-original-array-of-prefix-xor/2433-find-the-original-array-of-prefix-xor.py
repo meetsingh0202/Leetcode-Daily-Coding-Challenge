c76b7b3d46be4ef08b4686411a6095eb490ccdfe
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2023-10-31 09:53:45 +0530

    Time: 671 ms (46.26%), Space: 34.7 MB (83.96%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -1,43 +1,11 @@
 class Solution:
-    def findArray(self, pref: List[int]) -> List[int]:
-        
-        def convert(curr, target):
-            
-            tempVal = 0
-            
-            for i in range(31, -1, -1):
-                
-                if curr & (1 << i):
-                    x = 1
-                else:
-                    x = 0
-                    
-                if target & (1 << i):
-                    y = 1
-                else:
-                    y = 0
-                
-                if x == 1 and y == 1:
-                    tempVal = tempVal & ~(1 << i)
-                    
-                elif x == 1 and y == 0:
-                    tempVal = tempVal | (1 << i)            
-                    
-                elif x == 0 and y == 0:
-                    tempVal = tempVal & ~(1 << i)
-                    
-                elif x == 0 and y == 1:
-                    tempVal = tempVal | (1 << i)
-                
-            return tempVal
-        
+    def findArray(self, pref: List[int]) -> List[int]:       
         
         arr = [pref[0]]
         currXor = pref[0]
         
         for i in pref[1:]:
-            tempAns = convert(currXor, i)
-            currXor = currXor ^ tempAns
-            arr.append(tempAns)
+            arr.append(currXor ^ i)
+            currXor = currXor ^ (currXor ^ i)
         
         return arr
