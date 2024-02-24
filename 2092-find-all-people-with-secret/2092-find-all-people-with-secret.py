da3120c0d4bbdd6759449818ef9747030100e7cc
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2024-02-24 11:12:20 +0530

    Time: 1618 ms (58.28%), Space: 61 MB (44.37%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        meetings.sort(key = lambda x:x[2])
+        meeting_dict = collections.defaultdict(list)
+
+        for person1, person2, time in meetings:
+            meeting_dict[time].append([person1,person2])
+        
+        has_secrete = {0,firstPerson}
+
+        for time, meetings in meeting_dict.items():
+            graph = collections.defaultdict(list)
+
+            seen = set()
+
+            for person1, person2 in meetings:
+                graph[person1].append(person2)
+                graph[person2].append(person1)
+
+                if person1 in has_secrete:
+                    seen.add(person1)
+
+                if person2 in has_secrete:
+                    seen.add(person2)
+            
+            queue = collections.deque(seen)
+            while queue:
+                person = queue.popleft()
+
+                for neighbor in graph[person]:
+                    if neighbor not in has_secrete:
+                        has_secrete.add(neighbor)
+                        queue.append(neighbor)
+        return has_secrete
