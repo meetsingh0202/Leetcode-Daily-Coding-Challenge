926100795232d29cd5488308bef4a42ede6d01dd
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2024-10-29 14:54:53 +0530

    Time: 134 ms (60.61%), Space: 43.3 MB (5.30%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -1,17 +1,32 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        @cache
-        def dp(i: int, j: int) -> int:
-            if i < 0 or i >= m or j < 0 or j >= n: return 0
-            res = 0 
-            cur = grid[i][j]
-            for x, y in [(-1, 1), (0, 1), (1, 1)]:
-                if 0 <= i + x < m and 0 <= j + y < n and grid[i + x][j + y] > cur:
-                    res = max(res, dp(i + x, j + y) + 1)
-            return res
         
-        ans = 0
-        for i in range(m):
-            ans = max(ans, dp(i, 0))
-        return ans
+        @cache
+        def traverse(row, col):
+            
+            if row >= ROWS or col >= COLS:
+                return 0
+            
+            currVal = grid[row][col]
+            
+            up, down, right = 0, 0, 0
+            
+            if row + 1 < ROWS and col + 1 < COLS and grid[row + 1][col + 1] > currVal:
+                up = 1 + traverse(row + 1, col + 1)
+            
+            if col + 1 < COLS and grid[row][col + 1] > currVal:
+                right = 1 + traverse(row, col + 1)
+            
+            if row - 1 >= 0 and col + 1 < COLS and grid[row - 1][col + 1] > currVal:
+                down = 1 + traverse(row - 1, col + 1)
+            
+            return max(up, right, down)
+        
+        res = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        for i in range(ROWS):
+            res = max(res, traverse(i, 0))
+            
+        return res
+            
